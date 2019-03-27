4d26ea99677a45b2d08b9a3573ce8943303ad3e9
Mehmet Enes PAZAR <enespazar4342@gmail.com>
2019-03-27 12:00:15 +0300

    Publish Version 1.0.6

--- PictureComparison.TestConsole/Program.cs
+++ PictureComparison.TestConsole/Program.cs
@@ -18,8 +18,7 @@ namespace PictureComparison.TestConsole
                 @"C:/resim4.png",
                 @"C:/resim5.png",
                 @"C:/resim6.png",
-                @"C:/resim7.png",
-
+                @"C:/resim7.png"
             };
             //1. Parameter => way of main picture
             //2. Parameter => ways of other pictures
@@ -31,6 +30,11 @@ namespace PictureComparison.TestConsole
             //2. Parameter => ways of other pictures
             //Returns the paths and similarity ratios of the Dictionary <string, double>
             var result2 = PictureSimilarity.ComparePictures(mainPicture, otherPictures);
+
+            //Parameter => Ways of all pictures
+            //The image based on the backward img1 parameter returns the percentage of the similarity
+            //rate in the picture and SimilarityRatio parameter compared to img1 in the img2 parameter.
+            var result3 = PictureSimilarity.ComparePictures(otherPictures);
         }
     }
 }
